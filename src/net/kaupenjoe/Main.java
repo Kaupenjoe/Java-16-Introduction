e3e055acf7912c86489deaba8f6d425b09e583eb
Kaupenjoe <N.Kaupenjohann@gmail.com>
2021-11-20 16:36:35 +0100

    output and input

--- src/net/kaupenjoe/Main.java
+++ src/net/kaupenjoe/Main.java
@@ -1,31 +1,22 @@
 package net.kaupenjoe;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        /* HOW TO READ CODE / Java Syntax */
+        /* Outputting with println and Inputting with Scanner */
 
-        // Code is read from top to bottom
+        System.out.println("Hello Students!");
 
-        // This is a single line Comment
-
-        /*
-         *
-         * This is a
-         * Multiline Comment!
-         *
-         * */
-
-        // Declaring a Variable | Note that all lines end with a semi-colon!
-        int i;
-
-        // Assigning a Variable
-        i = 30;
-
-        // Declaring and Assigning (Initializing)
         int x = 10;
+        System.out.println(x);
 
-        // Access variables/method of Objects
-        String s = "Hello World";
-        s.length();
+        System.out.println("Outputting some Value: " + x);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Type in your Username");
+        String input = scanner.next();
+
+        System.out.println("Your Username is: " + input);
     }
 }
