37192b7991c92b1c06acc823e14d209fb933e0dd
Kaupenjoe <N.Kaupenjohann@gmail.com>
2021-11-21 18:45:40 +0100

    casting

--- src/net/kaupenjoe/Main.java
+++ src/net/kaupenjoe/Main.java
@@ -2,37 +2,12 @@ package net.kaupenjoe;
 
 public class Main {
     public static void main(String[] args) {
-        /* STRINGS AND STRING-METHODS */
+        /* CASTING (or converting data types between each other)  */
 
-        String sentence = "It's a wonderful Java Introduction!";
-        System.out.println(sentence);
-        System.out.println("Length is: " + sentence.length());
+        int loan = 750;
+        float interestRate = 0.0525f;
 
-        // Upper and Lower Case
-        System.out.println("SHOUTING: " + sentence.toUpperCase());
-        System.out.println("whispering: " + sentence.toLowerCase());
-        System.out.println(sentence);
-
-        // indexOf
-        System.out.println("The word 'Java' is found at position: " + sentence.indexOf("Java"));
-
-        // Replace
-        System.out.println(sentence.replace("Java", "C#"));
-
-        System.out.println(sentence.startsWith("I"));
-        System.out.println(sentence.endsWith("A"));
-
-        String s = "";
-        boolean empty = s.isEmpty();
-        System.out.println(empty);
-
-        // contains
-        System.out.println(sentence.contains("Java"));
-
-        // charAt
-        System.out.println(sentence.charAt(17));
-
-        // substring
-        System.out.println(sentence.substring(17));
+        float interest = (float)loan * interestRate;
+        System.out.println(interest);
     }
 }
