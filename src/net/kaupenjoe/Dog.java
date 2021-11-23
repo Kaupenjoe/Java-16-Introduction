81fc4ff09997e0a2a7eed3d1f7ccd207b4413f85
Kaupenjoe <N.Kaupenjohann@gmail.com>
2021-11-23 14:30:35 +0100

    inheritance

--- src/net/kaupenjoe/Dog.java
+++ src/net/kaupenjoe/Dog.java
@@ -1,41 +1,18 @@
 package net.kaupenjoe;
 
-public class Dog {
-    // Adding some fields
-    public String picture;
-    protected String name;
-    private int age;
+public class Dog extends Animal {
     public static final int dogToHumanYearMultiplier = 7;
 
-    // public, protected, private
-    // static, final
-
-
-    public int getAge() {
-        return age;
-    }
-
-    // Default Constructor
-    public Dog() {
-
-    }
-
-    public void birthday() {
-        age++;
+    public Dog(String picture, String name, int age) {
+        super(picture, name, age);
     }
 
     public int getAgeInHumanYears() {
         return age * dogToHumanYearMultiplier;
     }
 
-    public Dog(String picture, String name, int age) {
-        this.picture = picture;
-        this.name = name;
-        this.age = age;
-    }
-
-    // Method
-    public void woof() {
+    @Override
+    public void makeSound() {
         System.out.println(this.name + " just woofed!");
     }
 }
--- src/net/kaupenjoe/Main.java
+++ src/net/kaupenjoe/Main.java
@@ -1,26 +1,29 @@
 package net.kaupenjoe;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        /* Access Modifiers */
-
-        // used the default constructor
-        Dog doggo = new Dog();
-        System.out.println(doggo.name);
+        /* Inheritance & Polymorphism */
 
         Dog bengie = new Dog("bengie.png", "Bengie", 7);
-        System.out.println(bengie.name);
-        System.out.println(bengie.getAge());
-
         Dog gracie = new Dog("gracie.png", "Gracie", 5);
-        System.out.println(gracie.name);
-        System.out.println(gracie.getAge());
 
-        bengie.woof();
-        gracie.woof();
+        Cat whiskers = new Cat("whiskers.png", "Whiskers", 12);
 
-        // DOESN'T WORK!
-        // bengie.getAge() = 10;
-        System.out.println(bengie.getAgeInHumanYears());
+        whiskers.makeSound();
+        bengie.makeSound();
+        gracie.makeSound();
+
+        Animal animal = new Dog("jenny.png", "Jenny", 15);
+        animal.makeSound();
+
+        List<Animal> animals = new ArrayList<>();
+        animals.add(bengie);
+        animals.add(gracie);
+        animals.add(whiskers);
+
+        animals.get(1).makeSound();
     }
 }
