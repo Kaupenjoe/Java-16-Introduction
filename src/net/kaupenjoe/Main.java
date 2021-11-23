408eff02024e87a62dfe96288b206b231579a96a
Kaupenjoe <N.Kaupenjohann@gmail.com>
2021-11-23 15:40:24 +0100

    anonymous class

--- src/net/kaupenjoe/Main.java
+++ src/net/kaupenjoe/Main.java
@@ -1,35 +1,21 @@
 package net.kaupenjoe;
 
-import net.kaupenjoe.banking.Bank;
 import net.kaupenjoe.banking.Person;
 
 public class Main {
     public static void main(String[] args) {
-        /* ASSIGNMENT 4 */
-
-        /*
-         * ASSIGNMENT 4:
-         * Bank Management System:
-         * Create a Bank Management System which has the following components:
-         * Person (which saves first, middle and last name, age and maybe a "social security number")
-         * Bank (which saves a List of customers and their Checking Accounts, maximum overdraw amount)
-         * Checking Account (should have a current value, methods for taking and depositing money)
-         * Opening a Checking Account should only work via a Bank and must require a certain amount of initial deposit.
-         *
-         *
-         */
+        /* ANONYMOUS CLASSES */
 
         Person nano = new Person("Nano", "Attack", 8);
-        Person daniel = new Person("Daniel", "Smith", 24);
-        Person marie = new Person("Marie", "Susan", "Jones", 28);
 
-        Bank kb = new Bank("KaupenBank");
-        kb.openBankAccount(nano, 200); // Not old Enough
+        Person manager = new Person("John", "Smith", 42) {
+            @Override
+            public String getFullName() {
+                return "Manager " + super.getFullName();
+            }
+        };
 
-        kb.openBankAccount(marie, 2000);
-        kb.openBankAccount(daniel, 10); // Deposit is not enough
-
-        System.out.println(kb.getAccountByPerson(marie).getBalance()); // 2000
-        kb.withdrawAmount(3000, marie); // Cannot withdraw over the limit!
+        System.out.println(nano.getFullName());
+        System.out.println(manager.getFullName());
     }
 }
