8c96201cab1ec6b611ad23f1b0b920f15f799553
Kaupenjoe <N.Kaupenjohann@gmail.com>
2021-11-23 15:08:44 +0100

    assignment 3

--- src/net/kaupenjoe/Main.java
+++ src/net/kaupenjoe/Main.java
@@ -1,38 +1,21 @@
 package net.kaupenjoe;
 
-import java.util.ArrayList;
-import java.util.List;
+import net.kaupenjoe.trivia.TriviaGame;
 
 public class Main {
     public static void main(String[] args) {
-        /* INTERFACES AND ABSTRACT */
+        /* ASSIGNMENT 3 */
 
-        Dog bengie = new Dog("bengie.png", "Bengie", 7);
-        Dog gracie = new Dog("gracie.png", "Gracie", 5);
+        /*
+         * ASSIGNMENT 3:
+         * Trivia Game with CLASSES and Objects:
+         * Modify the Trivia Game we have made in the previous two exercises in such a way to use Classes as well.
+         * Imagine possible making custom classes for Question Answer Pairs, using Lists to save them
+         *
+         */
 
-        Cat whiskers = new Cat("whiskers.png", "Whiskers", 12);
-
-        whiskers.makeSound();
-        bengie.makeSound();
-        gracie.makeSound();
-
-        Animal animal = new Dog("jenny.png", "Jenny", 15);
-        animal.makeSound();
-
-        List<Animal> animals = new ArrayList<>();
-        animals.add(bengie);
-        animals.add(gracie);
-        animals.add(whiskers);
-
-        animals.get(1).makeSound();
-
-        // Animal a = new Animal("x.png", "X", 10);
-
-        List<IPettable> pets = new ArrayList<>();
-        pets.add(bengie);
-        pets.add(whiskers);
-
-        pets.get(1).pet();
-
+        // Uncomment both to play game!
+        TriviaGame game = new TriviaGame();
+        game.startGame();
     }
 }
