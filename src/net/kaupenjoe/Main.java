27b964c5c110f7ffdfc1cb31ce8a405d551cae2d
Kaupenjoe <N.Kaupenjohann@gmail.com>
2021-11-23 15:14:57 +0100

    assignment 4

--- src/net/kaupenjoe/Main.java
+++ src/net/kaupenjoe/Main.java
@@ -1,21 +1,35 @@
 package net.kaupenjoe;
 
-import net.kaupenjoe.trivia.TriviaGame;
+import net.kaupenjoe.banking.Bank;
+import net.kaupenjoe.banking.Person;
 
 public class Main {
     public static void main(String[] args) {
-        /* ASSIGNMENT 3 */
+        /* ASSIGNMENT 4 */
 
         /*
-         * ASSIGNMENT 3:
-         * Trivia Game with CLASSES and Objects:
-         * Modify the Trivia Game we have made in the previous two exercises in such a way to use Classes as well.
-         * Imagine possible making custom classes for Question Answer Pairs, using Lists to save them
+         * ASSIGNMENT 4:
+         * Bank Management System:
+         * Create a Bank Management System which has the following components:
+         * Person (which saves first, middle and last name, age and maybe a "social security number")
+         * Bank (which saves a List of customers and their Checking Accounts, maximum overdraw amount)
+         * Checking Account (should have a current value, methods for taking and depositing money)
+         * Opening a Checking Account should only work via a Bank and must require a certain amount of initial deposit.
+         *
          *
          */
 
-        // Uncomment both to play game!
-        TriviaGame game = new TriviaGame();
-        game.startGame();
+        Person nano = new Person("Nano", "Attack", 8);
+        Person daniel = new Person("Daniel", "Smith", 24);
+        Person marie = new Person("Marie", "Susan", "Jones", 28);
+
+        Bank kb = new Bank("KaupenBank");
+        kb.openBankAccount(nano, 200); // Not old Enough
+
+        kb.openBankAccount(marie, 2000);
+        kb.openBankAccount(daniel, 10); // Deposit is not enough
+
+        System.out.println(kb.getAccountByPerson(marie).getBalance()); // 2000
+        kb.withdrawAmount(3000, marie); // Cannot withdraw over the limit!
     }
 }
