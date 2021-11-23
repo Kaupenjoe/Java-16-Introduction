7a1c5c64705ce86293f6a773b99f5d49e0be1c69
Kaupenjoe <N.Kaupenjohann@gmail.com>
2021-11-23 16:30:19 +0100

    lambda

--- src/net/kaupenjoe/Main.java
+++ src/net/kaupenjoe/Main.java
@@ -1,30 +1,49 @@
 package net.kaupenjoe;
 
 import java.util.Scanner;
+import java.util.concurrent.Callable;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 public class Main {
     public static void main(String[] args) {
-        /* EXCEPTIONS AND TRY & CATCH */
+        /* LAMBDA EXPRESSIONS */
 
-        Scanner scanner = new Scanner(System.in);
-        int i = scanner.nextInt();
+        // Functions as Variables
+        // Supplier, Consumer, Callable, Runnable, Function
 
-        // checkForZero(i);
+        // Supplier: no parameter, but return value
+        // () -> x
+        Supplier<Integer> supplier = () -> { return 10; };
+        System.out.println(supplier.get());
 
-        try {
-            checkForZero(i);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        } finally {
-            System.out.println("Finally");
+        // Consumer: one parameter, but not return value
+        // x -> ()
+        Consumer<String> consumer = (s) -> { System.out.println(s); };
+        consumer.accept("Hello World");
+
+        // Callable: no parameter, and return value
+        // () -> x throws Exception
+        Callable<Float> callable = () -> { return 100f / 0f; };
+
+        try
+        {
+            callable.call();
+        }
+        catch (Exception e)
+        {
+            System.out.println(e);
         }
 
-        System.out.println("This happens always");
-    }
+        // Runnable: no parameters, no return value
+        // () -> ()
+        Runnable runnable = () -> { System.out.println("Hello World again"); };
+        runnable.run();
 
-    private static void checkForZero(int number) throws TestException {
-        if (number == 0) {
-            throw new TestException("NUMBER IS ZERO!");
-        }
+        // Function: has parameter, and return value
+        // x -> y
+        Function<Double, Double> function = (r) -> { return r * 3.14159265; };
+        System.out.println(function.apply(10.0));
     }
 }
